7027cc8a836e502de7a09e8a4d697291c212cfc3
punsak incham <punsak@mfec.co.th>
2025-04-19 01:17:00 +0700

    - Edit script for configured group

--- configured_group.sh
+++ configured_group.sh
@@ -5,6 +5,8 @@ gp_name=$(echo "$current_dir" | cut -d'/' -f3)
 
 echo "Directory name: $gp_name"
 
+sed -i "s/VM_OWNER=.*/VM_OWNER=${gp_name}/g" config_aws.properties
+sed -i "s/CONFLUENT_CLOUD_ENVIRONMENT=.*/CONFLUENT_CLOUD_ENVIRONMENT=${gp_name}/g" config_aws.properties
 sed -i "s/Name = \"bastion\"/Name = \"${gp_name}\"/g" terraform/main.tf
 sed -i 's/name = "allow_ssh_\${random_id\.id\.id}"/name = "allow_ssh_'"$gp_name"'"/g' terraform/main.tf
 sed -i 's/key_name = "pub_\${random_id\.id\.id}"/key_name = "pub_'"$gp_name"'"/g' terraform/main.tf
